4a401bb575d929eb13b26276b0db2cff272663ae
Natalie Youssef <natalieyoussef@Natalies-MacBook-Air.local>
2024-03-24 17:46:00 -0700

    first commit

---
+++ src/RestaurantInfo.java
@@ -0,0 +1,15 @@
+public class RestaurantInfo {
+    public static void main(String[] args) {
+        String restaurant_name = "Olive Garden";
+        String address = "6191 Centennial Center Blvd";
+        String city = "Las Vegas";
+        String state = "Nevada";
+        String zip_code = "89149";
+
+        System.out.println("Restaurant Name: " + restaurant_name);
+        System.out.println("Address: " + address);
+        System.out.println("City: " + city);
+        System.out.println("State: " + state);
+        System.out.println("Zip code: " + zip_code);
+    }
+}
